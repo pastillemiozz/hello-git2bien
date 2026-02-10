ea32d5018ff1f80206e026155957824ce37b1c5b
pastillazz <robertoalonsopelayo26@gmail.com>
2026-02-10 18:20:25 -0500

    git version 3

--- hellogit2.java
+++ hellogit2.java
@@ -1,5 +1,5 @@
 public class hellogit2 {
     public static void main(String[] args) {
-        System.out.println("hola mamaguevo siuu");
+        System.out.println("hola mamaguevo siuu version 2 ");
     }
 }
