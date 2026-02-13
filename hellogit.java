8371670dcecafd06c94f02a66d42b5f8430246ac
pastillazz <robertoalonsopelayo26@gmail.com>
2026-02-13 17:04:10 -0500

    hola de nuevo

--- hellogit.java
+++ hellogit.java
@@ -1,6 +1,6 @@
 public class hellogit {
 
     public static void main(String[] args) {
-        System.out.println("hola git de nuevo sotronco de descarao ");
+        System.out.println("hola git de nuevo sotronco de descarao putii ");
     }
 }
