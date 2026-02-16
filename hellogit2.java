53bbaede1f94320321d16a1647c9f327872aea77
pastillazz <robertoalonsopelayo26@gmail.com>
2026-02-15 21:07:18 -0500

    arreglado v6

--- hellogit2.java
+++ hellogit2.java
@@ -2,5 +2,6 @@ public class hellogit2 {
     public static void main(String[] args) {
 
         System.out.println("hola mamaguevo siuu version 3 ");
+        System.out.println("se fue tu jeva");
     }
 }
