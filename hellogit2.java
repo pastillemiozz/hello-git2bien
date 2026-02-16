213fc2138d5ee98fc70a3e42b56eced4489e8947
pastillazz <robertoalonsopelayo26@gmail.com>
2026-02-15 19:01:51 -0500

    hola mamag v3

--- hellogit2.java
+++ hellogit2.java
@@ -1,6 +1,6 @@
 public class hellogit2 {
     public static void main(String[] args) {
 
-        System.out.println("hola mamaguevo siuu version 2 ");
+        System.out.println("hola mamaguevo siuu version 3 ");
     }
 }
